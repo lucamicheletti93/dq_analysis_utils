0a01985d805c678fa01be073101b948badd436d4
Lucamicheletti93 <luca.mike93@gmail.com>
2022-05-18 11:30:04 +0200

    Fitting the spectrum with the proper functions

--- grid/hyperloop/benchmark.py
+++ grid/hyperloop/benchmark.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
 import array as arr
+import numpy as np
 import os
 import sys
 import argparse
 import ROOT
 from os import path
-from ROOT import TCanvas, TF1, TFile, TPaveText
+from ROOT import TCanvas, TF1, TFile, TPaveText, TMath
 from ROOT import gROOT, gBenchmark, gPad, gStyle
 from ctypes import cdll
+from function_library import Func_Tot_ee, Func_Tot_mumu
 
 def params():
     production = ['LHC15o_test', 'LHC18_pp_test', 'LHC18_pp_benchmark']
@@ -58,9 +60,8 @@ def params():
 
     plt.show()
 
-def fit():
-    #lib = cdll.LoadLibrary('./../../../dq_fit_library/DQFitter_cxx.so')
 
+def fit():
     fIn_ee = TFile.Open("AnalysisResults_LHC18_dielectron.root")
     hlist_ee = fIn_ee.Get("analysis-same-event-pairing/output")
     list_ee = hlist_ee.FindObject("PairsBarrelSEPM_jpsiO2MCdebugCuts")
@@ -72,12 +73,20 @@ def fit():
         #histMass_ee.SetBinContent(histMass_ee.FindBin(2.5) + i, 0)
         #histMass_ee.SetBinError(histMass_ee.FindBin(2.5) + i, 0)
 
-    funcMass_ee = TF1("funcMass_ee", "gaus(0)+pol2(3)", 2, 5)
-    funcMass_ee.SetParameter(1, 3.1)
-    funcMass_ee.SetParameter(2, 0.10)
-    funcMass_ee.SetParameter(3, 1.1e4)
-    funcMass_ee.SetParameter(4, -3.e3)
-    funcMass_ee.SetParameter(5, 3.1e2)
+    # Input parameters for the fit to the e+e- spectrum
+    par_signal_ee = [100.,3.096,7.0e-02,0.089,10.393]
+
+    funcMass_ee = TF1("funcMass_ee", Func_Tot_ee, 2, 5, 7)
+    funcMass_ee.SetNpx(1000)
+    funcMass_ee.SetParameter(0,100.)
+    funcMass_ee.SetParameter(1,1.)
+    funcMass_ee.SetParameter(2,par_signal_ee[0])
+    funcMass_ee.SetParameter(3,par_signal_ee[1])
+    funcMass_ee.SetParName(3, "#mu_{J/#psi}")
+    funcMass_ee.SetParameter(4,par_signal_ee[2])
+    funcMass_ee.SetParName(4, "#sigma_{J/#psi}")
+    funcMass_ee.FixParameter(5,par_signal_ee[3])
+    funcMass_ee.FixParameter(6,par_signal_ee[4])
     histMass_ee.Fit(funcMass_ee, "R")
 
     fIn_mumu = TFile.Open("AnalysisResults_LHC18_dimuon.root")
@@ -91,14 +100,26 @@ def fit():
         #histMass_mumu.SetBinContent(histMass_mumu.FindBin(2.5) + i, 0)
         #histMass_mumu.SetBinError(histMass_mumu.FindBin(2.5) + i, 0)
     
-    funcMass_mumu = TF1("funcMass_mumu", "gaus(0)+pol4(3)", 2, 5)
-    funcMass_mumu.SetParameter(1, 3.1)
-    funcMass_mumu.SetParameter(2, 0.07)
-    funcMass_mumu.SetParameter(3, 7.1e4)
-    funcMass_mumu.SetParameter(4, 1.1e5)
-    funcMass_mumu.SetParameter(5, -4.3e4)
-    funcMass_mumu.SetParameter(6, 7.7e3)
-    funcMass_mumu.SetParameter(7, -5.0e2)
+    # Input parameters for the fit to the mu+mu- spectrum
+    par_signal_mumu = [100.,3.096,7.0e-02,1.089,3.393,2.097,8.694,0.01]
+
+    funcMass_mumu = TF1("funcMass_mumu", Func_Tot_mumu, 2.3, 4.5, 12)
+    funcMass_mumu.SetNpx(1000)
+    funcMass_mumu.FixParameter(0, 3.82888e+06)
+    funcMass_mumu.FixParameter(1, -1.92923e+03)
+    funcMass_mumu.FixParameter(2, 6.02176e+04)
+    funcMass_mumu.FixParameter(3, 5.96033e+04)
+    funcMass_mumu.SetParameter(4, 50000.)
+    funcMass_mumu.SetParameter(5, par_signal_mumu[1]) 
+    funcMass_mumu.SetParName(5, "#mu_{J/#psi}")
+    funcMass_mumu.SetParameter(6, par_signal_mumu[2])
+    funcMass_mumu.SetParName(6, "#sigma_{J/#psi}")
+    funcMass_mumu.FixParameter(7, par_signal_mumu[3])
+    funcMass_mumu.FixParameter(8, par_signal_mumu[4])
+    funcMass_mumu.FixParameter(9, par_signal_mumu[5])
+    funcMass_mumu.FixParameter(10, par_signal_mumu[6])
+    funcMass_mumu.SetParameter(11, par_signal_mumu[7])
+    funcMass_mumu.SetParName(11, "#psi(2S)/J/#psi")
     histMass_mumu.Fit(funcMass_mumu, "R")
 
     canvas = TCanvas("canvas", "canvas", 1200, 600)
@@ -115,8 +136,9 @@ def fit():
     gStyle.SetOptStat(0)
     gStyle.SetOptFit(1)
     histMass_mumu.Draw("EPsame")
+    funcMass_mumu.Draw("same")
 
-    canvas.Update()
+    canvas.Update() 
     input()
 
 
